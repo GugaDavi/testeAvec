0005e49138757c53540492c73b9dd3910c282198
GugaDavi <gguedes.tech@gmail.com>
2019-11-20 20:18:36 -0300

    Ajuste de constate global

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,10 +1,10 @@
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-import { primaryColor } from '../../util/consts';
+import { primaryColor, secundaryColor } from '../../util/consts';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#fff', primaryColor],
+  colors: [primaryColor, secundaryColor],
 })`
   flex: 1;
   color: rgba(2, 2, 2, 0.7);
