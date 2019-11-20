05323ea54769f26948dd0b26e70d0f8d170279ae
GugaDavi <gguedes.tech@gmail.com>
2019-11-20 20:19:54 -0300

    Dashboard Screen

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-import { Container } from './styles';
+import { Container, TitleView, Title, CitysList } from './styles';
+import Background from '../../components/Background';
+import Card from '../../components/CityCard';
+import NoCitys from '../../components/NoCitys';
 
-export default function Dashboard() {
+export default function Dashboard({ navigation }) {
+  const citys = useSelector(state => state.citys.citys);
+
+  useEffect(() => {}, [citys]);
+
+  function handleAbout() {
+    navigation.navigate('Localizations');
+  }
+
   return (
-    <Container>
-      <Text>Hello World</Text>
-    </Container>
+    <Background>
+      <TitleView>
+        <Title>Cidades</Title>
+      </TitleView>
+      <Container>
+        {citys.length > 0 ? (
+          <CitysList
+            data={citys}
+            keyExtractor={(city, index) => String(index)}
+            renderItem={({ item }) => (
+              <Card cityName={item.name} onPress={() => handleAbout(item.id)} />
+            )}
+          />
+        ) : (
+          <NoCitys />
+        )}
+      </Container>
+    </Background>
   );
 }
+
+Dashboard.propTypes = {
+  navigation: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+    .isRequired,
+};
--- src/screens/Dashboard/styles.js
+++ src/screens/Dashboard/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const TitleView = styled.View`
+  padding-top: 20px;
+`;
+export const Title = styled.Text`
+  font-size: 32px;
+  color: #fff;
+  text-align: center;
+`;
+export const Container = styled.ScrollView`
+  padding: 20px 10px 0px 10px;
+`;
+
+export const CitysList = styled.FlatList.attrs({
+  showVerticalScollIndicator: false,
+})`
+  margin-bottom: 90px;
+`;
